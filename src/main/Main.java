4e7b2f7a007ea39b93144562d2cdee7b03a97ca3
Jelena Smiljanic <jelenasmiljanic3@gmail.com>
2020-01-18 14:50:56 +0100

    Citanje excel fajla

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,49 +1,50 @@
 package main;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(new File("C:\\demo\\student.xls"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        HSSFWorkbook wb = null;
-        try {
-            wb = new HSSFWorkbook(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        HSSFSheet sheet = wb.getSheetAt(0);
-
-        FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
-        for (Row row : sheet) {
-            for (Cell cell : row) {
-                switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
-                    case Cell.CELL_TYPE_NUMERIC:
-
-                        System.out.print(cell.getNumericCellValue() + "\t\t");
-                        break;
-                    case Cell.CELL_TYPE_STRING:
-
-                        System.out.print(cell.getStringCellValue() + "\t\t");
-                        break;
+        try
+        {
+            File file = new File("C:\\Users\\jesm\\Downloads\\ASM_PZ2_podaci_1920 (1)\\UB_cs_authors.xlsx");   //creating a new file instance
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
+//creating Workbook instance that refers to .xlsx file
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            Iterator<Sheet> sheets = wb.iterator();
+            while(sheets.hasNext()) {
+                XSSFSheet sheet = (XSSFSheet) sheets.next();     //creating a Sheet object to retrieve object
+                Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+                while (itr.hasNext()) {
+                    Row row = itr.next();
+                    Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        switch (cell.getCellType()) {
+                            case Cell.CELL_TYPE_STRING:    //field that represents string cell type
+                                System.out.print(cell.getStringCellValue() + "\t\t\t");
+                                break;
+                            case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type
+                                System.out.print(cell.getNumericCellValue() + "\t\t\t");
+                                break;
+                            default:
+                        }
+                    }
+                    System.out.println("");
                 }
             }
-            System.out.println();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
