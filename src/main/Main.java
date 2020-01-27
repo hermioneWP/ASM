509aec4f814a79f3380d28a3df0bdb83a78ea615
TAASMJE1 <jelena.smiljanic@swisscom.com>
2020-01-27 09:52:50 +0100

    Sredila exel fajlove da budu u resources. Napravila listu i HashMapu autora

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static org.apache.poi.ss.usermodel.CellType.*;
@@ -20,42 +23,10 @@ import static org.apache.poi.ss.usermodel.CellType.STRING;
 public class Main {
 
     public static void main(String[] args) {
-        List<Autor> autori = new ArrayList<>();
-        int id = 0;
-        try {
-            File file = new File("C:\\Users\\jesm\\Downloads\\ASM_PZ2_podaci_1920 (1)\\UB_cs_authors.xlsx");   //creating a new file instance
-            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
-//creating Workbook instance that refers to .xlsx file
-            XSSFWorkbook wb = new XSSFWorkbook(fis);
-            Iterator<Sheet> sheets = wb.iterator();
-            while (sheets.hasNext()) {
-                XSSFSheet sheet = (XSSFSheet) sheets.next();     //creating a Sheet object to retrieve object
-                Iterator<Row> itr = sheet.iterator();    //iterating over excel file
-                Row row2 = itr.next();
-                while (itr.hasNext()) {
-                    Row row = itr.next();
+        List<Autor> authors = new ArrayList<>();
+        HashMap<String,Autor> authorsMap = new HashMap<>();
+        generateAuthors(authors,authorsMap);
 
-                    Cell cell = row.getCell(0);
-                    switch (cell.getCellTypeEnum()) {
-                        case STRING:    //field that represents string cell type
-                            System.out.print(cell.getStringCellValue() + "\t\t\t");
-                            autori.add(new Autor(id++, cell.getStringCellValue() + " " + row.getCell(1).getStringCellValue()));
-                            break;
-                        case _NONE:
-                            break;
-                        case NUMERIC:    //field that represents number cell type
-                            System.out.print(cell.getNumericCellValue() + "\t\t\t");
-                            break;
-                        default:
-                    }
-                    System.out.println("");
-                }
-
-            }
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
         XSSFWorkbook workbook = new XSSFWorkbook();
 
         //Create a blank sheet
@@ -68,10 +39,10 @@ public class Main {
         int rowid = 0;
         row = spreadsheet.createRow(rowid++);
         Cell naslov1 = row.createCell(0);
-        naslov1.setCellValue("ID");
+        naslov1.setCellValue("Id");
         Cell naslov2 = row.createCell(1);
-        naslov2.setCellValue("Ime");
-        for (Autor a : autori) {
+        naslov2.setCellValue("Label");
+        for (Autor a : authors) {
             row = spreadsheet.createRow(rowid++);
 
             Cell cell = row.createCell(0);
@@ -85,13 +56,49 @@ public class Main {
         //Write the workbook in file system
         try {
             FileOutputStream out = new FileOutputStream(
-                    new File("C:\\Users\\jesm\\Desktop\\Autori.xlsx"));
+                    new File("izlazFajlovi\\Autori.xlsx"));
             workbook.write(out);
             out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Writesheet.xlsx written successfully");
+        System.out.println("Autori.xlsx written successfully");
+    }
+
+    public static void generateAuthors(List<Autor> authors,HashMap<String,Autor> authorsMap){
+        try {
+            int id = 0;
+            File file = new File("resources\\UB_cs_authors.xlsx");
+            FileInputStream fis = new FileInputStream(file);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            Iterator<Sheet> sheets = wb.iterator();
+            while (sheets.hasNext()) {
+                XSSFSheet sheet = (XSSFSheet) sheets.next();     //creating a Sheet object to retrieve object
+                Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+                Row row2 = itr.next();
+                while (itr.hasNext()) {
+                    Row row = itr.next();
+
+                    Cell cell = row.getCell(0);
+                    switch (cell.getCellTypeEnum()) {
+                        case STRING:    //field that represents string cell type
+                            Autor newAuthor = new Autor(id++, (row.getCell(1).getStringCellValue()+" "+cell.getStringCellValue().charAt(0)).toLowerCase());
+                            authors.add(newAuthor);
+                            authorsMap.put(newAuthor.getIme(),newAuthor);
+                            break;
+                        case _NONE:
+                            break;
+                        case NUMERIC:
+                            break;
+                        default:
+                    }
+                }
+
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
     }
 }
